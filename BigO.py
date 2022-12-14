5c5c514b59e799d20da81a236aca60803d965a4c
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2022-12-13 17:29:13 -0800

    classes and pointers

---
+++ BigO.py
@@ -0,0 +1,22 @@
+'''
+best: omega, average:theta, worst:omicron
+n = 1,000
+O(1) = 1 constant
+O(logn) = 10
+O(n) = 1,000 proportional
+O(nlogn) = 10,000 Divide and conquer
+O(n^2) = 1,000,000 loop within a loop
+
+O(2^n)
+O(n!)
+'''
+
+# different terms for inputs:  O(a*b) not O(n)
+def print_items(a,b):
+    for i in range(a):
+        print(i)
+
+    for j in range(b):
+        print(j)
+
+print_items(1, 10)
---
+++ Pointer.py
@@ -0,0 +1,48 @@
+num1 = 11
+
+num2 = num1 # point to same address
+
+print("Before num2 value is updated:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+print("\nnum1 points to:", id(num1)) # take the address id()
+print("num2 points to:", id(num2)) 
+
+num2 = 22 # integer are immutable, num2 point to new integer with different address
+
+print("\nAfter num2 value is updated:") 
+print("num1 =", num1) # 11 
+print("num2 =", num2)  # 22
+
+# different address
+print("\nnum1 points to:", id(num1))
+print("num2 points to:", id(num2))
+
+
+#####################################
+
+
+dict1 = {
+         'value': 11
+        }
+
+dict2 = dict1  # point to same dict
+
+print("\n\nBefore value is updated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+
+print("\ndict1 points to:", id(dict1))
+print("dict2 points to:", id(dict2)) 
+
+dict2['value'] = 22 # not point to a new dict, just overwrite value
+
+print("\nAfter value is updated:") 
+print("dict1 =", dict1) # 22
+print("dict2 =", dict2) # 22
+
+print("\ndict1 points to:", id(dict1))# still same address
+print("dict2 points to:", id(dict2))
+
+
